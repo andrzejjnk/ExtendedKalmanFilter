51ec6c07da34b99027369671ed4615a8801d437a
Andrzej Janik <rkeere2017@gmail.com>
2024-11-15 17:59:55 +0100

    Little testing update

--- ekf_test.py
+++ ekf_test.py
@@ -19,12 +19,12 @@ magData = data[['MagX', 'MagY', 'MagZ']].values * 1000  # Zmiana jednostek magne
 num_stationary_samples = 400
 
 # Krok 1: Kalibracja żyroskopu
-gyro_bias = np.mean(gyroData[:num_stationary_samples], axis=0)
+gyro_bias = np.mean(gyroData[:1000], axis=0)
 gyroData_calibrated = gyroData - gyro_bias
 
 # Krok 2: Kalibracja akcelerometru
 # Zakładamy, że w stanie spoczynku przyspieszenie w osi Z = 9.81, a w osiach X i Y = 0
-acc_bias = np.mean(accData[:num_stationary_samples], axis=0) - np.array([0, 0, 9.81])
+acc_bias = np.mean(accData[:1000], axis=0) - np.array([0, 0, 9.81])
 accData_calibrated = accData - acc_bias
 
 # Krok 3: Kalibracja magnetometru
@@ -62,30 +62,6 @@ def quaternion_to_euler(q):
     return np.degrees(roll), np.degrees(pitch), np.degrees(yaw)
 
 
-def euler_from_quaternion(q):
-        """
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        w, x, y, z = q
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-     
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-     
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-     
-        return roll_x, pitch_y, yaw_z # in radians
-
-
 output = []
 for i, q in enumerate(quaternions):
     roll, pitch, yaw = quaternion_to_euler(q)
@@ -107,7 +83,7 @@ df = pd.DataFrame(data)
 df.to_csv("orientation_test_output.csv", index=False, float_format="%.2f")
 
 
-def plot_angles(computed_angles, csv_file):
+def plot_angles(csv_file):
     data = pd.read_csv(csv_file)
     
     roll_data = data['roll']
@@ -152,7 +128,7 @@ def plot_angles(computed_angles, csv_file):
     plt.show()
 
 
-plot_angles(quaternions, 'data/train.csv')
+plot_angles('data/train.csv')
 
 def calculate_mse(original_csv, computed_csv):
     # Load original and computed data
